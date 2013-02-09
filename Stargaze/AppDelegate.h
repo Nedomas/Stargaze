4dd807699c47cf8d8b671dd715e0496169b6bb59
Nedomas <domas.bitvinskas@me.com>
2013-02-09 16:46:51 +0200

    Initial devbox commit

---
+++ Stargaze/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Stargaze
+//
+//  Created by Domas Bitvinskas on 2013-02-09.
+//  Copyright (c) 2013 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Stargaze/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Stargaze
+//
+//  Created by Domas Bitvinskas on 2013-02-09.
+//  Copyright (c) 2013 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
